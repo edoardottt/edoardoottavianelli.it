b402ffc427f1c640e71edfa99ed8e9ab0b57af56
edoardottt <edoardott@gmail.com>
2021-05-01 18:48:27 +0200

    update

---
+++ js/footer.js
@@ -0,0 +1,66 @@
+document.getElementById("footer").innerHTML = `
+<!--Call to action-->
+<div class="pt-4">
+  <a class="btn btn-outline-white" href="mailto:edoardott@gmail.com" target="_blank" role="button"
+    title="mail edoardo ottavianelli">Contact me
+    <i class="fas fa-envelope ml-2"></i>
+  </a>
+</div>
+<!--/.Call to action-->
+
+<hr class="my-4">
+
+<!-- Social icons -->
+<div class="pb-4">
+  <a href="https://www.facebook.com/edoardo.ottavianelli" target="_blank" title="facebook edoardo ottavianelli">
+    <i class="fab fa-facebook-f mr-3"></i>
+  </a>
+
+  <a href="https://twitter.com/edoardottt2" target="_blank" title="twitter edoardo ottavianelli">
+    <i class="fab fa-twitter mr-3"></i>
+  </a>
+
+  <a href="https://github.com/edoardottt" target="_blank" title="github edoardo ottavianelli">
+    <i class="fab fa-github mr-3"></i>
+  </a>
+
+  <a href="https://instagram.com/edoardottt" target="_blank" title="instagram edoardo ottavianelli">
+    <i class="fab fa-instagram mr-3"></i>
+  </a>
+
+  <a href="https://www.linkedin.com/in/edoardoottavianelli/" target="_blank" title="linkedin edoardo ottavianelli">
+    <i class="fab fa-linkedin mr-3"></i>
+  </a>
+
+  <a href="https://t.me/edoardottt" target="_blank" title="telegram edoardo ottavianelli">
+    <i class="fab fa-telegram mr-3"></i>
+  </a>
+
+  <a href="https://stackoverflow.com/users/8155097/edoardottt" target="_blank"
+    title="stackoverflow edoardo ottavianelli">
+    <i class="fab fa-stack-overflow mr-3"></i>
+  </a>
+
+  <a href="https://www.youtube.com/channel/UCBoJMSbkCGdardyMyuYNyHA" title="edoardo ottavianelli youtube"
+    target="_blank">
+    <i class="fab fa-youtube mr-3"></i>
+  </a>
+
+  <a href="https://www.hackerrank.com/edoardottt" title="edoardo ottavianelli hackerrank" target="_blank">
+    <i class="fab fa-hackerrank mr-3"></i>
+  </a>
+
+  <a href="https://dev.to/edoardottt" title="edoardo ottavianelli dev.to" target="_blank">
+    <i class="fab fa-dev mr-3"></i>
+  </a>
+
+</div>
+<!-- Social icons -->
+
+<!--Copyright-->
+<div class="footer-copyright py-3">
+  &#xA9; edoardottt <br>Thanks to
+  <a href="https://mdbootstrap.com/education/bootstrap/" target="_blank"> MDBootstrap.com </a>
+</div>
+<!--/.Copyright-->
+`
